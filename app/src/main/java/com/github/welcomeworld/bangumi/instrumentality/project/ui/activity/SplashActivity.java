9ec10690940f7c66ac07bc2cb93322e35e0a6114
welcomeworld <guduziyousiwang@gmail.com>
2022-03-26 18:38:17 +0800

    更新闪屏页

--- app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/ui/activity/SplashActivity.java
+++ app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/ui/activity/SplashActivity.java
@@ -8,18 +8,23 @@ import com.github.welcomeworld.bangumi.instrumentality.project.databinding.Activ
 import com.github.welcomeworld.bangumi.instrumentality.project.utils.IntentUtil;
 import com.nisigada.common.devbase.utils.ThreadUtil;
 
-public class SplashActivity extends BaseActivity<ActivitySplashBinding>{
+public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ThreadUtil.postDelayed(3000, new Runnable() {
+        ThreadUtil.postDelayed(500, new Runnable() {
             @Override
             public void run() {
-                if(isTaskRoot()){
-                    IntentUtil.intentToMain(SplashActivity.this,null);
+                if (isTaskRoot()) {
+                    IntentUtil.intentToMain(SplashActivity.this, null);
                 }
                 finish();
             }
         });
+    }
+
+    @Override
+    public void refreshSystemUIVisibility() {
+        //do nothing
     }
 }
