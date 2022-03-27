ad6fa6d6f99fe78285b8fc8a0c7f69669267eb0d
welcomeworld <guduziyousiwang@gmail.com>
2022-03-27 22:23:09 +0800

    加入备用更新线路

--- app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/ui/fragment/SettingsFragment.java
@@ -11,9 +11,11 @@ import com.github.welcomeworld.bangumi.instrumentality.project.BuildConfig;
 import com.github.welcomeworld.bangumi.instrumentality.project.R;
 import com.github.welcomeworld.bangumi.instrumentality.project.databinding.FragmentSettingsBinding;
 import com.github.welcomeworld.bangumi.instrumentality.project.utils.ScreenUtil;
+import com.nisigada.common.devbase.utils.KVUtil;
 
 public class SettingsFragment extends BaseFragment<FragmentSettingsBinding> implements View.OnClickListener {
     public static String TAG = "setting_fragment";
+    private static final String SETTING_BACKUP_UPDATE = "setting_backup_update";
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -22,12 +24,18 @@ public class SettingsFragment extends BaseFragment<FragmentSettingsBinding> impl
         getViewBinding().settingVersionName.setText(BuildConfig.VERSION_NAME);
         getViewBinding().settingVersion.setOnClickListener(this);
         getViewBinding().settingOpensource.setOnClickListener(this);
+        getViewBinding().settingBackupCheck.setChecked(KVUtil.getBoolean(SETTING_BACKUP_UPDATE));
+        getViewBinding().settingBackupCheck.setOnCheckedChangeListener((buttonView, isChecked) -> KVUtil.putBoolean(SETTING_BACKUP_UPDATE, isChecked));
     }
 
     public void onClick(View view) {
         int id = view.getId();
         if (id == R.id.setting_version) {
-            UpdateManager.checkUpdate(view.getContext(),"https://gitee.com/nisigada/software-update/raw/master/bip/version.json",BuildConfig.VERSION_NAME);
+            if (KVUtil.getBoolean(SETTING_BACKUP_UPDATE)) {
+                UpdateManager.checkUpdate(view.getContext(), "https://gitee.com/nisigada/software-update/raw/master/bip/version_bak.json", BuildConfig.VERSION_NAME);
+            } else {
+                UpdateManager.checkUpdate(view.getContext(), "https://gitee.com/nisigada/software-update/raw/master/bip/version.json", BuildConfig.VERSION_NAME);
+            }
         } else if (id == R.id.setting_opensource) {
 
         }
