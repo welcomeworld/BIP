09e18fa228f1f6b1d5cf3988192f984cfc51cec1
welcomeworld <guduziyousiwang@gmail.com>
2023-07-09 14:35:17 +0800

    fixed import class lost

--- app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/ui/widget/BatteryView.java
+++ app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/ui/widget/BatteryView.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
