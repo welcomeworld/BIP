7fe3db22c34e3144bc0c7eacfdc554c10c6ec6cb
welcomeworld <guduziyousiwang@gmail.com>
2023-07-20 09:06:41 +0800

    optimize quality window
    (cherry picked from commit 7096e1308bbc8f9c1effd574fa002c5599e637e3)

---
+++ app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/adapter/QualityRvAdapter.java
@@ -0,0 +1,61 @@
+package com.github.welcomeworld.bangumi.instrumentality.project.adapter;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.welcomeworld.bangumi.instrumentality.project.databinding.RvQualityItemBinding;
+import com.github.welcomeworld.bangumi.instrumentality.project.model.VideoQualityBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QualityRvAdapter extends RecyclerView.Adapter<QualityRvAdapter.QualityHolder> {
+    List<VideoQualityBean> data = new ArrayList<>();
+    public ItemClickListener itemClickListener;
+
+    @NonNull
+    @Override
+    public QualityHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new QualityHolder(RvQualityItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QualityHolder holder, int position) {
+        VideoQualityBean qualityBean = data.get(position);
+        holder.binding.qualityItem.setText(qualityBean.getQuality());
+        holder.binding.qualityItem.setOnClickListener(v -> {
+            if (itemClickListener != null) {
+                itemClickListener.onItemClick(position, qualityBean);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void replaceAll(List<VideoQualityBean> data) {
+        this.data.clear();
+        this.data.addAll(data);
+        notifyDataSetChanged();
+    }
+
+    static class QualityHolder extends RecyclerView.ViewHolder {
+        RvQualityItemBinding binding;
+
+        public QualityHolder(RvQualityItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(int position, VideoQualityBean qualityBean);
+    }
+}
