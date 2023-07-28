ac35316f26484e85b9b5676199ee3d9946a54e2c
welcomeworld <guduziyousiwang@gmail.com>
2023-07-28 08:51:48 +0800

    optimize video play UI

---
+++ app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/adapter/VideoDescRVAdapter.java
@@ -0,0 +1,100 @@
+package com.github.welcomeworld.bangumi.instrumentality.project.adapter;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.welcomeworld.bangumi.instrumentality.project.databinding.RvVideoPlaySourceHeaderBinding;
+import com.github.welcomeworld.bangumi.instrumentality.project.model.VideoListBean;
+import com.github.welcomeworld.devbase.utils.StringUtil;
+
+public class VideoDescRVAdapter extends RecyclerView.Adapter<VideoDescRVAdapter.HeaderItemHolder> {
+    VideoListBean data;
+    VideoDescRVAdapter.ActionClickListener actionClickListener;
+    private boolean isFav = false;
+
+    @NonNull
+    @Override
+    public VideoDescRVAdapter.HeaderItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new VideoDescRVAdapter.HeaderItemHolder(RvVideoPlaySourceHeaderBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoDescRVAdapter.HeaderItemHolder itemHolder, int position) {
+        itemHolder.binding.videoPlayTitle.setText(data.getTitle());
+        if (!StringUtil.isEmpty(data.getVideoListDes())) {
+            itemHolder.binding.videoPlayDes.setText(data.getVideoListDes());
+        }
+        itemHolder.binding.videoPlayCollection.setSelected(isFav);
+        itemHolder.binding.videoPlayCollection.setOnClickListener(v -> {
+            isFav = !isFav;
+            itemHolder.binding.videoPlayCollection.setSelected(isFav);
+            if (actionClickListener != null) {
+                actionClickListener.onFavClick();
+            }
+        });
+        itemHolder.binding.videoPlayDownload.setOnClickListener(v -> {
+            if (actionClickListener != null) {
+                actionClickListener.onDownloadClick();
+            }
+        });
+        itemHolder.binding.videoPlayBrowser.setOnClickListener(v -> {
+            if (actionClickListener != null) {
+                actionClickListener.onBrowserClick();
+            }
+        });
+        itemHolder.binding.videoPlayRefresh.setOnClickListener(v -> {
+            if (actionClickListener != null) {
+                actionClickListener.onRefreshClick();
+            }
+        });
+        itemHolder.binding.videoPlayComment.setOnClickListener(v -> {
+            if (actionClickListener != null) {
+                actionClickListener.onCommentClick();
+            }
+        });
+    }
+
+    static class HeaderItemHolder extends RecyclerView.ViewHolder {
+        RvVideoPlaySourceHeaderBinding binding;
+
+        public HeaderItemHolder(RvVideoPlaySourceHeaderBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return data == null ? 0 : 1;
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void setData(VideoListBean data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    public interface ActionClickListener {
+        void onFavClick();
+
+        void onDownloadClick();
+
+        void onBrowserClick();
+
+        void onRefreshClick();
+
+        void onCommentClick();
+    }
+
+    public void setActionClickListener(VideoDescRVAdapter.ActionClickListener actionClickListener) {
+        this.actionClickListener = actionClickListener;
+    }
+
+    public void setFav(boolean fav) {
+        isFav = fav;
+    }
+}
--- app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/adapter/VideoSourceItemAdapter.java
+++ app/src/main/java/com/github/welcomeworld/bangumi/instrumentality/project/adapter/VideoSourceItemAdapter.java
@@ -13,73 +13,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.github.welcomeworld.bangumi.instrumentality.project.R;
 import com.github.welcomeworld.bangumi.instrumentality.project.adapter.decoration.SpaceItemDecoration;
 import com.github.welcomeworld.bangumi.instrumentality.project.model.VideoListBean;
-import com.github.welcomeworld.bangumi.instrumentality.project.utils.LogUtil;
-import com.github.welcomeworld.devbase.utils.StringUtil;
 
 import java.util.List;
 
 public class VideoSourceItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     List<VideoListBean> data;
     ItemClickListener itemClickListener;
-    ActionClickListener actionClickListener;
-    private boolean isFav = false;
     private int selectSourceIndex = -1;
-
-    private static final int HEADER_TYPE = 233;
-    private static final int ITEM_TYPE = 234;
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType == HEADER_TYPE) {
-            return new HeaderItemHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_video_play_source_header, parent, false));
-
-        }
         return new ItemHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_video_play_source, parent, false));
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if (position == 0) {
-            return HEADER_TYPE;
-        } else {
-            return ITEM_TYPE;
-        }
-    }
-
-    @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof HeaderItemHolder) {
-            LogUtil.e("SourceAdapter", data.get(position).toString());
-            HeaderItemHolder itemHolder = (HeaderItemHolder) holder;
-            itemHolder.videoPlayTitle.setText(data.get(position).getTitle());
-            if (!StringUtil.isEmpty(data.get(position).getVideoListDes())) {
-                itemHolder.videoPlayDes.setText(data.get(position).getVideoListDes());
-            }
-            itemHolder.favView.setSelected(isFav);
-            itemHolder.favView.setOnClickListener(v -> {
-                isFav = !isFav;
-                itemHolder.favView.setSelected(isFav);
-                if (actionClickListener != null) {
-                    actionClickListener.onFavClick();
-                }
-            });
-            itemHolder.downloadView.setOnClickListener(v -> {
-                if (actionClickListener != null) {
-                    actionClickListener.onDownloadClick();
-                }
-            });
-            itemHolder.browserView.setOnClickListener(v -> {
-                if (actionClickListener != null) {
-                    actionClickListener.onBrowserClick();
-                }
-            });
-            itemHolder.refreshView.setOnClickListener(v -> {
-                if (actionClickListener != null) {
-                    actionClickListener.onRefreshClick();
-                }
-            });
-        }
         ItemHolder itemHolder = (ItemHolder) holder;
         if (data.size() <= 1 && data.get(0).getVideoBeanList().size() <= 1) {
             itemHolder.videoItemRecyclerView.setVisibility(View.GONE);
@@ -118,26 +67,6 @@ public class VideoSourceItemAdapter extends RecyclerView.Adapter<RecyclerView.Vi
         }
     }
 
-    static class HeaderItemHolder extends ItemHolder {
-        TextView videoPlayTitle;
-        TextView videoPlayDes;
-        TextView favView;
-        TextView downloadView;
-        TextView browserView;
-        TextView refreshView;
-
-        public HeaderItemHolder(@NonNull View itemView) {
-            super(itemView);
-            videoPlayTitle = itemView.findViewById(R.id.video_play_title);
-            videoPlayDes = itemView.findViewById(R.id.video_play_des);
-            favView = itemView.findViewById(R.id.video_play_collection);
-            downloadView = itemView.findViewById(R.id.video_play_download);
-            browserView = itemView.findViewById(R.id.video_play_browser);
-            refreshView = itemView.findViewById(R.id.video_play_refresh);
-
-        }
-    }
-
     @Override
     public int getItemCount() {
         return data == null ? 0 : data.size();
@@ -161,21 +90,6 @@ public class VideoSourceItemAdapter extends RecyclerView.Adapter<RecyclerView.Vi
         void onItemClick(RecyclerView rv, int sourcePosition, int position);
     }
 
-    public interface ActionClickListener {
-        void onFavClick();
-
-        void onDownloadClick();
-
-        void onBrowserClick();
-
-        void onRefreshClick();
-
-    }
-
-    public void setActionClickListener(ActionClickListener actionClickListener) {
-        this.actionClickListener = actionClickListener;
-    }
-
     public int getSelectSourceIndex() {
         return selectSourceIndex;
     }
@@ -184,9 +98,5 @@ public class VideoSourceItemAdapter extends RecyclerView.Adapter<RecyclerView.Vi
         int oldIndex = this.selectSourceIndex;
         this.selectSourceIndex = selectSourceIndex;
         notifyItemChanged(oldIndex);
-    }
-
-    public void setFav(boolean fav) {
-        isFav = fav;
     }
 }
