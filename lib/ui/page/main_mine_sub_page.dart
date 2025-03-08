aad590c9c6f8ce415f914305717ba3e0fe9689fc
welcomeworld <guduziyousiwang@gmail.com>
2025-03-08 16:43:14 +0800

    refactor code

--- lib/ui/page/main_mine_sub_page.dart
+++ lib/ui/page/main_mine_sub_page.dart
@@ -1,3 +1,4 @@
+import 'package:bip/data/model/user_info.dart';
 import 'package:bip/ui/widgets/simple_svg.dart';
 import 'package:flutter/material.dart';
 
@@ -26,84 +27,11 @@ class _MainMineSubPageState
               if (accounts == null) {
                 return const SizedBox.shrink();
               }
-              final accountInfo = accounts.entries.map((entry) {
-                final MapEntry(key: sourceName, value: userInfo) = entry;
-                if (userInfo == null) {
-                  return SizedBox(
-                    height: 80,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: FilledButton(
-                            onPressed: () {},
-                            style: FilledButton.styleFrom(
-                              shape: const CircleBorder(),
-                              fixedSize: const Size(60, 60),
-                              padding: EdgeInsets.zero,
-                            ),
-                            child: Text(
-                              localeString.login,
-                              style: TextStyle(
-                                color: colorScheme.onPrimary,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          sourceName,
-                          style: TextStyle(
-                            color: colorScheme.onSurface,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                } else {
-                  return SizedBox(
-                    height: 80,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: FilledButton(
-                            onPressed: () {},
-                            style: FilledButton.styleFrom(
-                              shape: const CircleBorder(),
-                              fixedSize: const Size(60, 60),
-                              padding: EdgeInsets.zero,
-                            ),
-                            child: CircleAvatar(
-                              radius: 26,
-                              backgroundImage: userInfo.avatar.isNotEmpty
-                                  ? NetworkImage(userInfo.avatar)
-                                  : const AssetImage(
-                                      "assets/img/ic_default_avatar.png",
-                                    ),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          userInfo.name,
-                          style: TextStyle(
-                            color: colorScheme.onSurface,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                }
-              }).toList();
               return Column(
                 mainAxisSize: MainAxisSize.min,
-                children: accountInfo,
+                children: accounts.entries
+                    .map((entry) => _userInfo(entry.key, entry.value))
+                    .toList(),
               );
             }),
         Padding(
@@ -114,142 +42,148 @@ class _MainMineSubPageState
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                IconButton(
-                  style: IconButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16))),
-                  onPressed: () {},
-                  icon: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SimpleSvg(
-                        "assets/img/ic_download.svg",
-                        size: 30,
-                        color: colorScheme.secondary,
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        localeString.download,
-                        style: TextStyle(color: colorScheme.onSurface),
-                      ),
-                    ],
-                  ),
+                _topIconButton(
+                  "assets/img/ic_download.svg",
+                  localeString.download,
+                  () {},
                 ),
-                IconButton(
-                  style: IconButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16))),
-                  onPressed: () {},
-                  icon: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SimpleSvg(
-                        "assets/img/ic_favorite_outline.svg",
-                        size: 30,
-                        color: colorScheme.secondary,
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        localeString.favorite,
-                        style: TextStyle(color: colorScheme.onSurface),
-                      ),
-                    ],
-                  ),
+                _topIconButton(
+                  "assets/img/ic_favorite_outline.svg",
+                  localeString.favorite,
+                  () {},
                 ),
-                IconButton(
-                  style: IconButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16))),
-                  onPressed: () {},
-                  icon: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SimpleSvg(
-                        "assets/img/ic_history_outline.svg",
-                        size: 30,
-                        color: colorScheme.secondary,
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        localeString.history,
-                        style: TextStyle(color: colorScheme.onSurface),
-                      ),
-                    ],
-                  ),
+                _topIconButton(
+                  "assets/img/ic_history_outline.svg",
+                  localeString.history,
+                  () {},
                 ),
-                IconButton(
-                  style: IconButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16))),
-                  onPressed: () {},
-                  icon: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SimpleSvg(
-                        "assets/img/ic_message.svg",
-                        size: 30,
-                        color: colorScheme.secondary,
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        localeString.message,
-                        style: TextStyle(color: colorScheme.onSurface),
-                      ),
-                    ],
-                  ),
+                _topIconButton(
+                  "assets/img/ic_message.svg",
+                  localeString.message,
+                  () {},
                 ),
               ],
             ),
           ),
         ),
-        ListTile(
-          leading: SimpleSvg(
-            "assets/img/ic_theme.svg",
-            size: 24,
-            color: colorScheme.tertiary,
-          ),
-          trailing: SimpleSvg(
-            "assets/img/ic_right_arrow.svg",
-            size: 16,
-            color: colorScheme.onSurface.withOpacity(0.6),
-          ),
-          onTap: () {},
-          title: Text(
-            localeString.theme,
-            style: TextStyle(color: colorScheme.onSurface),
-          ),
+        _listTileItem(
+          "assets/img/ic_theme.svg",
+          localeString.theme,
+          () {},
         ),
-        ListTile(
-          leading: SimpleSvg(
-            "assets/img/ic_settings.svg",
-            size: 24,
-            color: colorScheme.tertiary,
-          ),
-          onTap: () {},
-          title: Text(
-            localeString.settings,
-            style: TextStyle(color: colorScheme.onSurface),
-          ),
-          trailing: SimpleSvg(
-            "assets/img/ic_right_arrow.svg",
-            size: 16,
-            color: colorScheme.onSurface.withOpacity(0.6),
-          ),
+        _listTileItem(
+          "assets/img/ic_settings.svg",
+          localeString.settings,
+          () {},
         ),
         const SizedBox(height: 24),
       ],
+    );
+  }
+
+  Widget _topIconButton(
+    String iconAsset,
+    String label,
+    VoidCallback? onPressed,
+  ) {
+    return IconButton(
+      style: IconButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16))),
+      onPressed: onPressed,
+      icon: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SimpleSvg(
+            iconAsset,
+            size: 30,
+            color: colorScheme.secondary,
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            label,
+            style: TextStyle(color: colorScheme.onSurface),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _userInfo(String sourceName, UserInfo? userInfo) {
+    final isLogin = userInfo != null;
+    return IconButton(
+      onPressed: () {},
+      style: IconButton.styleFrom(
+          minimumSize: const Size(0, 80),
+          padding: const EdgeInsets.only(left: 16, right: 24)),
+      icon: Row(children: [
+        isLogin
+            ? CircleAvatar(
+                radius: 32,
+                backgroundImage: NetworkImage(userInfo.avatar),
+              )
+            : Ink(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorScheme.primary,
+                ),
+                width: 64,
+                height: 64,
+                child: Center(
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    localeString.login,
+                    style: TextStyle(
+                      color: colorScheme.onPrimary,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Text(
+            isLogin ? userInfo.name : sourceName,
+            style: TextStyle(
+              color: colorScheme.onSurface,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        SimpleSvg(
+          "assets/img/ic_right_arrow.svg",
+          size: 16,
+          color: colorScheme.onSurface.withOpacity(0.6),
+        ),
+      ]),
+    );
+  }
+
+  Widget _listTileItem(
+    String iconAsset,
+    String label,
+    VoidCallback? onPressed,
+  ) {
+    return ListTile(
+      leading: SimpleSvg(
+        iconAsset,
+        size: 24,
+        color: colorScheme.tertiary,
+      ),
+      onTap: onPressed,
+      title: Text(
+        label,
+        style: TextStyle(color: colorScheme.onSurface),
+      ),
+      trailing: SimpleSvg(
+        "assets/img/ic_right_arrow.svg",
+        size: 16,
+        color: colorScheme.onSurface.withOpacity(0.6),
+      ),
     );
   }
 }
