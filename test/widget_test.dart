1550ba321ea782e9aac2d3218ef07bea66747b8b
welcomeworld <guduziyousiwang@gmail.com>
2025-04-20 15:30:28 +0800

    remove unused test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,8 +10,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bip/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-  });
+
 }
